d23795e961f3b451dd020fd08a07477e747d253c
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-10-20 11:27:32 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -21,7 +21,7 @@ function Login() {
       }
       else{
     try {
-      const response = await fetch("https://leavemanagementapp.onrender.com/api/login", {
+      const response = await fetch("https://leave-management-app-4kpz.vercel.app/api/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
